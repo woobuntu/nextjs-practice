0ca0d71b39bf6d0bf87697f5f916c8d04b940eb9
woobuntu <boutime2017@gmail.com>
2021-07-25 15:50:02 +0900

    동적 라우팅으로의 navigating

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 function ClientPage() {
+  const clients = [
+    { id: "max", name: "max" },
+    { id: "manu", name: "manu" },
+  ];
   return (
     <div>
       <h1>클라이언트 페이지</h1>
+      <ul>
+        {clients.map(({ id, name }) => (
+          <li key={id}>
+            <Link href={`/clients/${name}`}>{name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
