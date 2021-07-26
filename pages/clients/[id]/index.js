dfd1137315a71454485638d899625cb6250ded4b
in uh, lee <boutime2017@gmail.com>
2021-07-26 18:04:21 +0900

    Link컴포넌트를 사용하지 않고 navigate하는 방법

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,27 @@
+import { useRouter } from "next/router";
+
 function ClientProjectPage() {
+  const router = useRouter();
+
+  function loadProjectHandler() {
+    // data를 load하는 등의 작업 후
+    router.push("/clients/max/projecta");
+    // router.push({
+    //   pathname: "/clients/[id]/[clientProjectId]",
+    //   query: {
+    //     id: "max",
+    //     clientProjectId: "projecta",
+    //   },
+    // });
+    // router.replace를 사용할 경우 뒤로 가기를 사용할 수 없다.
+    // 일반적으로는 Link로 navigating하지만,
+    // form submit등의 상황에서는 이와 같은 방법으로 navigate할 수 있다.
+  }
+
   return (
     <div>
       <h1>해당 클라이언트의 프로젝트들</h1>
+      <button onClick={loadProjectHandler}>프로젝트 a</button>
     </div>
   );
 }
