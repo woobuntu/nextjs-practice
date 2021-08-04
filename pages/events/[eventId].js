ef2dd69d6835e4e64c0d6080e86bf8deabae45d7
in uh, lee <boutime2017@gmail.com>
2021-08-04 11:09:15 +0900

    동적 라우팅 페이지 구성

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,13 +1,33 @@
 import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/EventSummary";
+import EventLogistics from "../../components/event-detail/EventLogistics";
+import EventContent from "../../components/event-detail/EventContent";
 
 function EventDetailPage() {
   const {
     query: { eventId },
   } = useRouter();
+
+  const eventDetail = getEventById(eventId);
+
+  if (!eventDetail) return <p>No event found!</p>;
+
+  const { title, description, location, date, image } = eventDetail;
+
+  const logisticsProps = {
+    date,
+    address: location,
+    image,
+    imageAlt: title,
+  };
+
   return (
-    <div>
-      <h1>EventDetailPage - {eventId}</h1>
-    </div>
+    <>
+      <EventSummary title={title} />
+      <EventLogistics {...logisticsProps} />
+      <EventContent>{description}</EventContent>
+    </>
   );
 }
 
