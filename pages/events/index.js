83224c6a62cede705281c584a2c867bf2625713a
in uh, lee <boutime2017@gmail.com>
2021-08-05 12:42:08 +0900

    모든 이벤트 받아오기

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,17 +1,12 @@
-const dummyEvents = ["event1", "event2", "event3"];
-import Link from "next/link";
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/EventList";
 
 function EventsPage() {
+  const events = getAllEvents();
+
   return (
     <div>
-      <h1>EventsPage</h1>
-      <ul>
-        {dummyEvents.map((event, index) => (
-          <li key={index}>
-            <Link href={`/events/${event}`}>{event}</Link>
-          </li>
-        ))}
-      </ul>
+      <EventList eventList={events} />
     </div>
   );
 }
